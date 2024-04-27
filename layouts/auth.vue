b4678249ff7fc3a7aed164a27da2327683cdddf8
kato emma <emmanuelkato39@gmail.com>
2024-04-27 14:24:38 +0300

    initial commit with loin form and auth layout

---
+++ layouts/auth.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+<template>
+    <div class="p-1 bg-red-400">
+        <div class="bg-[#1ea896] h-fit 2xl:h-screen container mx-auto p-1 md:p-20 rounded-xl">
+            <div class="bg-white rounded-xl h-full flex flex-col md:flex-row justify-center items-center rounded-xl">
+                <div class="hidden md:block  w-full rounded-l-xl "
+                    style="background-image: url('https://asanify.com/wp-content/uploads/2021/07/picxy_TF531665.jpg');background-size: cover; background-position: center;">
+                    <div class="bg-black/60 h-full rounded-l-xl flex flex-col justify-center p-10">
+                        <h1 class="text-xl md:text-3xl lg:text-5xl font-bold text-[#1ea896] ">Kato's HRMS</h1>
+                        <h2 class="text-lg md:text-xl   text-white ">A system to manage all employees on a
+                            single platform
+                        </h2>
+                        <p class="text-white mt-8">
+                            This Sytem is being developed in Vue-Nuxt, Tailwindcss and Supabase
+                        </p>
+                    </div>
+                </div>
+
+                <div class="w-full h-full bg-gray-200 rounded-r-xl">
+                    <div class="flex flex-col p-10">
+                        <!-- logo -->
+                        <img src="/images/logo.png" class="w-16" alt="">
+                        <!-- login form -->
+                        <slot />
+
+                    </div>
+                    
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+definePageMeta({
+    layout: 'auth'
+})
+
+</script>
+<template>
+    <div class="mt-10 bg-white p-5 rounded-lg shadow-md mx-auto w-full max-w-md">
+        <h1 class="text-2xl font-bold">Login</h1>
+        <form action="" class="mt-10">
+            <div class="flex flex-col mt-10">
+                <label for="email" class="text-sm font-semibold">Email</label>
+                <input type="email" name="email" id="email" class="border-2 border-[#1ea896] rounded-md p-2 mt-2"
+                    placeholder="Enter your email">
+            </div>
+            <div class="flex flex-col mt-10">
+                <label for="password" class="text-sm font-semibold">Password</label>
+                <input type="password" name="password" id="password"
+                    class="border-2 border-[#1ea896] rounded-md p-2 mt-2" placeholder="Enter your password">
+            </div>
+            <div class="flex flex-col mt-10">
+                <label for="password" class="text-sm font-semibold">Remember Me</label>
+                <input type="checkbox" name="remember" id="remember"
+                    class="border-2 border-[#1ea896] rounded-md p-2 mt-2">
+            </div>
+            <div class="flex flex-col mt-10">
+                <button type="submit"
+                    class="bg-[#1ea896] text-white rounded-md p-2 hover:bg-[#1ea896]/80">Login</button>
+            </div>
+        </form>
+    </div>
+</template>
